89a9c199add4aa60d6669d9e44f690c5e77bdb1a
mxmoss <mxmossman@gmail.com>
2015-12-14 19:59:08 -0800

    cleaned up list

--- avclublist.py
+++ avclublist.py
@@ -9,11 +9,15 @@ import datetime
 import urllib.request		# this for python 3 and later
 import requests
 
-amzSearch = '<a target="_blank" href="http://www.amazon.com/s/ref=as_li_ss_tl?_encoding=UTF8&camp=1789&creative=390957&field-keywords={0}&linkCode=ur2&tag=moss0e7-20&url=search-alias%3Daps&{1}</a><img src="https://ir-na.amazon-adsystem.com/e/ir?t=moss0e7-20&l=ur2&o=1" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />'
-
-'''
-amzSearch = '<a target="_blank" href="http://www.amazon.com/s/ref=as_li_ss_tl?_encoding=UTF8&camp=1789&creative=390957&field-keywords={0}&linkCode=ur2&tag=moss0e7-20&url=search-alias%3Daps&linkId=4JTWP2QOJSYBL7NU">{1}</a><img src="https://ir-na.amazon-adsystem.com/e/ir?t=moss0e7-20&l=ur2&o=1" width="1" height="1" border="0" alt="" style="border:none !important; margin:0px !important;" />'
-'''
+#template for an amazon link
+amzSearch="<a target='_blank' href='http://www.amazon.com/s/ref=as_li_ss_tl?_encoding=UTF8&\
+camp=1789&\
+creative=390957&\
+field-keywords={0} {1}&\
+linkCode=ur2&\
+tag=moss0e7-20&\
+url=search-alias%3Daps&\
+linkId=JHSX46QUDWJ2CG2M'>{2} {3}</a>"
 
 myURL = "http://www.avclub.com/article/our-favorite-graphic-novels-one-shots-and-archives-229423"
 page = urllib.request.urlopen(myURL)
@@ -27,7 +31,8 @@ rows = soup.find_all('h4')
 #output
 print('<html><head><title>Books to read</title></head><body><ul>')
 for row in rows:
-  print('<li>'+amzSearch+'</li>'.format(row.i, row.i))
+  print('<li>')
+  #output title, author
+  print(amzSearch.format(row.contents[1].string, row.contents[2].string, row.i, row.contents[2].string))
+  print('</li>')
 print('</ul></body></html>')
-
-
